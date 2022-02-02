c01291518af0ee78ffa02aa19bba4ca5eada51b3
DylanTN01 <60158152+DylanTN01@users.noreply.github.com>
2022-02-01 18:59:05 -0600

    Add files via upload

---
+++ CSVParsingTest2.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CSVParsingTest2 {
+    @Test
+        void test(){
+            CSVParsing CSVParsing = new CSVParsing();
+            String line = CSVParsing.readDataLineByLine("Data2.csv");
+            assertEquals(line, "name rollno department result cgpa aman 23 cse pass 8.9 rahul 45 ee fail 4.6");
+            fail("Test Failed");
+        }
+}
